fe438a59ab5323592dcc7dba3c4278ec4172e0d0
antlau2000 <61512568+antlau2000@users.noreply.github.com>
2023-02-09 21:30:41 +0300

    hibernate commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -22,7 +25,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
 
+        Session session = Util.getSessionJavaConfigFactory().getCurrentSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+
+        Util.getSessionJavaConfigFactory().close();
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,16 @@
 package jm.task.core.jdbc.util;
 
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Util {
@@ -15,8 +21,54 @@ public class Util {
                     "&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "root";
+    private static SessionFactory sessionJavaConfigFactory;
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getJDBCConnection() throws SQLException {
         return DriverManager.getConnection(URL_FIXED, USERNAME, PASSWORD);
+    }
+
+    public static void closeJDBCConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static SessionFactory buildSessionJavaConfigFactory() {
+        try {
+            Configuration configuration = new Configuration();
+
+            //Create Properties, can be read from property files too
+            Properties properties = new Properties();
+            properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            properties.put("hibernate.connection.url", URL);
+            properties.put("hibernate.connection.username", USERNAME);
+            properties.put("hibernate.connection.password", PASSWORD);
+            properties.put("hibernate.current_session_context_class", "thread");
+
+            configuration.setProperties(properties);
+
+            //we can set mapping file or class with annotation
+            //addClass(Employee1.class) will look for resource
+            // com/journaldev/hibernate/model/Employee1.hbm.xml (not good)
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Java Config serviceRegistry created");
+
+            return configuration.buildSessionFactory(serviceRegistry);
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionJavaConfigFactory() {
+        if(sessionJavaConfigFactory == null) {
+            sessionJavaConfigFactory = buildSessionJavaConfigFactory();
+        }
+        return sessionJavaConfigFactory;
     }
 }
